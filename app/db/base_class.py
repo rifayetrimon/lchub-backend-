c7b41ed80ef824d2d7dfbbd117116ae7b86e6452
rifayet <rifayetrimon88@gmail.com>
2025-06-25 16:47:01 +0800

    add base class model

---
+++ app/db/base_class.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import  Column, Integer, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.sql.operators import truediv
+
+
+@as_declarative()
+class Base:
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
---
+++ app/models/users.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+import enum
