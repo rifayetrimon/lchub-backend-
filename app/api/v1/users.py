306ce13dc6b98a593090566533476535a0a8d948
rifayet <rifayetrimon88@gmail.com>
2025-07-01 11:56:35 +0800

    add login function

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 from starlette import status
 from app.db.session import get_db
-from app.schemas.users import UserResponse, UserCreate, UserRead, Token
+from app.schemas.users import UserResponse, UserCreate, UserRead, Token, UserLogin
 from app.services.users import UserService
 from app.core.security import verify_password, create_access_token
 
@@ -29,6 +29,20 @@ async def login_for_access_token(
     db: AsyncSession = Depends(get_db)
 ):
     user = await UserService.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    token = create_access_token(data={"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
+
+
+
+@router.post("/login", response_model=Token)
+async def login_user(user_in: UserLogin, db: AsyncSession = Depends(get_db)):
+    user = await UserService.authenticate_user(user_in.email, user_in.password, db)
 
     if not user:
         raise HTTPException(
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -64,3 +64,7 @@ class UserResponse(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
