130b4171bba1b36e614a5a6eb808d8ebffb370e7
rifayet <rifayetrimon88@gmail.com>
2025-07-01 21:23:55 +0800

    add service category service

--- app/services/service_categories.py
+++ app/services/service_categories.py
@@ -2,9 +2,9 @@ from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 import logging
 from app.models.users import User
-from app.models.service import Service
+from app.models.service_categories import ServiceCategory
 from fastapi import HTTPException, status
-from app.schemas.service import CreateService, ServiceResponse
+from app.schemas.service_categories import CreateServiceCategory
 from sqlalchemy import and_
 
 
@@ -24,10 +24,45 @@ class ServiceCategoriesService:
             )
 
 
-    # @staticmethod
-    # async def create_service_category(user: User, service_data: CreateService, db: AsyncSession):
-    #     try:
-    #
+    @staticmethod
+    async def create_service_category(service_category_data: CreateServiceCategory, db: AsyncSession, user: User):
+        try:
+            await ServiceCategoriesService._verify_user_authorization(user)
+
+            existing_category = await db.execute(
+                select(ServiceCategory).where(
+                    and_(
+                        ServiceCategory.name == service_category_data.name,
+                    )
+                )
+            )
+
+            if existing_category.scalar():
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail="Category already exists",
+                )
+
+            new_service_category = ServiceCategory(
+                **service_category_data.model(exclude={"user_id"}),
+                user_id = user.id
+            )
+
+            try:
+                db.add(new_service_category)
+                db.commit()
+                db.refresh(new_service_category)
+
+            except Exception as e:
+                await db.rollback()
+                logger.error(f"Failed to create new service_category: {str(e)}")
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail="Failed to create new service_category",
+                )
+
+            return new_service_category
+
 
 
 
