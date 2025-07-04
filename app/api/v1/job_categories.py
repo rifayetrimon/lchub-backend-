abd091b7dce148e90b435f82d19bb02e7d040dde
rifayet <rifayetrimon88@gmail.com>
2025-07-04 10:16:08 +0800

    add updante and delete api for job categories

--- app/api/v1/job_categories.py
+++ app/api/v1/job_categories.py
@@ -1,9 +1,9 @@
 from typing import List
-
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
+from h11 import Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
-from app.schemas.job_categories import CreateJobCategory, ReadJobCategory
+from app.schemas.job_categories import CreateJobCategory, ReadJobCategory, UpdateJobCategory
 from app.services.job_categories import JobCategoriesService
 
 router = APIRouter(prefix="/job_categories", tags=["job_categories"])
@@ -17,13 +17,27 @@ async def create_job_category(
     return new_job_category
 
 
-@router.get("/", response_model=List[ReadJobCategory])
+@router.get("/", response_model=List[ReadJobCategory], status_code=status.HTTP_200_OK)
 async def get_job_categories(db: AsyncSession = Depends(get_db)):
     job_categories = await JobCategoriesService.get_job_categories(db)
     return job_categories
 
 
-@router.get("/{id}", response_model=ReadJobCategory)
+@router.get("/{id}", response_model=ReadJobCategory, status_code=status.HTTP_200_OK)
 async def get_job_category(id: int, db: AsyncSession = Depends(get_db)):
     job_category = await JobCategoriesService.get_job_category_by_id(id, db)
     return job_category
+
+
+
+@router.put("/{id}", response_model=UpdateJobCategory, status_code=status.HTTP_200_OK)
+async def update_job_category(id: int, job_category_data: UpdateJobCategory,db: AsyncSession = Depends(get_db)):
+   update_job_category = await JobCategoriesService.update_job_category(id, job_category_data, db)
+   return update_job_category
+
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_job_category(id: int, db: AsyncSession = Depends(get_db)):
+    await JobCategoriesService.delete_job_category(id, db)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/services/job_categories.py
+++ app/services/job_categories.py
@@ -3,7 +3,7 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy import and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from app.schemas.job_categories import CreateJobCategory
+from app.schemas.job_categories import CreateJobCategory, UpdateJobCategory
 from app.schemas.service import BaseService
 from app.models.job_categories import JobCategory
 
@@ -87,4 +87,64 @@ class JobCategoriesService(BaseService):
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Failed to get job categories",
+            )
+
+
+    @staticmethod
+    async def update_job_category(job_category_id: int ,job_category_date: UpdateJobCategory, db: AsyncSession):
+        try:
+            result = await db.execute(
+                select(JobCategory).where(JobCategory.id == job_category_id)
+            )
+
+            category = result.scalar_one_or_none()
+
+            if not category:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Job category not found",
+                )
+
+            for key, value in job_category_date.model_dump().items():
+                setattr(category, key, value)
+
+
+            db.add(category)
+            await db.commit()
+            await db.refresh(category)
+            return category
+
+        except Exception as e:
+            logger.error(f"Failed to update job category: {str(e)}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to update job category",
+            )
+
+
+    @staticmethod
+    async def delete_job_category(job_category_id: int, db: AsyncSession):
+        try:
+            job_category = await db.execute(
+                select(JobCategory).where(JobCategory.id == job_category_id)
+            )
+
+            category = job_category.scalar_one_or_none()
+
+            if not category:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Job category not found",
+                )
+
+            await db.delete(category)
+            await db.commit()
+            await db.refresh(category)
+            return category
+
+        except Exception as e:
+            logger.error(f"Failed to delete job category: {str(e)}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to delete job category",
             )
